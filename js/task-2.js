eb9a8854f7efa98b5ae0a64df23cbd19e98b550b
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-02-27 23:45:04 +0200

    make task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,12 @@
 'use strict';
+function formatMessage(message, maxLengthrams) {
+  if (message <= maxLengthrams) return message;
+  else return message.slice(0, maxLengthrams) + '...';
+}
+
+console.log(formatMessage('Curabitur ligula sapien', 16));
+console.log(formatMessage('Curabitur ligula sapien', 23));
+console.log(formatMessage('Vestibulum facilisis purus nec', 20));
+console.log(formatMessage('Vestibulum facilisis purus nec', 30));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
